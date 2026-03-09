b934edf8f8297ef4f37d24e5625dff7584fd2efb
Joery van Egmond <joeryvanegmond@hotmail.com>
2026-03-09 21:25:39 +0100

    whoops

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,7 @@ use Inertia\Inertia;
 class HomeController extends Controller
 {
     public function index(GvbService $service){
-        // $data = $service->getDepartures('NL:S:30009902');
-        $data = [];
+        $data = $service->getDepartures('NL:S:30009902');
         return Inertia::render('Home', [
             'line' => $data[0]['line'] ?? "F9",
             'destination'=> $data[0]['destination'] ?? "Centraal Station",
